844291679525f7ba795822357baac7f51db6e691
yjc5726 <yjc5726@hotmail.com>
2020-11-28 17:13:43 +0800

    heroku init

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 const mongoose = require('mongoose') // 載入 mongoose
-const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars')
 const Record = require('./models/record') // 載入 Record model
 const Category = require('./models/category') // 載入 Category model
 
 // 引用 body-parser
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/expense-tracker'
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true }) // 設定連線到 mongoDB
@@ -24,8 +24,7 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-
-app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -35,9 +34,8 @@ app.get('/', (req, res) => {
   Record.find() // 取出 Record model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .sort({ date: 'desc' })
-    .then((records) => res.render('index', { records, array: ['home','transportation','entertainment','food','other'] }))
+    .then((records) => res.render('index', { records, array: ['home', 'transportation', 'entertainment', 'food', 'other'] }))
     .catch(error => console.error(error))
-  
 })
 
 app.get('/records/new', (req, res) => {
@@ -45,8 +43,8 @@ app.get('/records/new', (req, res) => {
 })
 
 app.post('/records', (req, res) => {
-  const {name, category, date, amount } = req.body       // 從 req.body 拿出表單裡的 name, category, date, amount 資料
-  return Record.create({ name, category, date, amount })     // 存入資料庫
+  const { name, category, date, amount } = req.body // 從 req.body 拿出表單裡的 name, category, date, amount 資料
+  return Record.create({ name, category, date, amount }) // 存入資料庫
     .then(() => res.redirect('/')) // 新增完成後導回首頁
     .catch(error => console.log(error))
 })
@@ -55,13 +53,13 @@ app.get('/records/:id/edit', (req, res) => {
   const id = req.params.id
   return Record.findById(id)
     .lean()
-    .then((record) => res.render('edit', { record, array: ['home','transportation','entertainment','food','other'] }))
+    .then((record) => res.render('edit', { record, array: ['home', 'transportation', 'entertainment', 'food', 'other'] }))
     .catch(error => console.log(error))
 })
 
 app.post('/records/:id/edit', (req, res) => {
   const id = req.params.id
- const {name, category, date, amount } = req.body
+  const { name, category, date, amount } = req.body
   return Record.findById(id)
     .then(record => {
       record.name = name
@@ -70,7 +68,7 @@ app.post('/records/:id/edit', (req, res) => {
       record.amount = amount
       return record.save()
     })
-    .then(()=> res.redirect('/'))
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
@@ -84,15 +82,13 @@ app.post('/records/:id/delete', (req, res) => {
 
 app.get('/search', (req, res) => {
   const keyword = req.query.category
-   Record.find() // 取出 Record model 裡的所有資料
+  Record.find() // 取出 Record model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .find({category: `${keyword}`})
-    .then(records => res.render('index', { records, keyword, array: ['home','transportation','entertainment','food','other']})) // 將資料傳給 index 樣板
+    .find({ category: `${keyword}` })
+    .then(records => res.render('index', { records, keyword, array: ['home', 'transportation', 'entertainment', 'food', 'other'] })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
-  
-
 })
 // 設定 port 3000
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -8,30 +8,27 @@ db.on('error', () => {
 })
 
 db.once('open', () => {
-  
-    Category.create({
-      name: 'home',
-      ch_name: '家居物業',
-      icon:'https://fontawesome.com/icons/home?style=solid'
-    }, {
-      name: 'transportation',
-      ch_name: '交通出行',
-      icon:'https://fontawesome.com/icons/shuttle-van?style=solid'
-    }, {
-      name: 'entertainment',
-      ch_name: '休閒娛樂',
-      icon:'https://fontawesome.com/icons/grin-beam?style=solid'
-    }, {
-      name: 'food',
-      ch_name: '餐飲食品',
-      icon:'https://fontawesome.com/icons/utensils?style=solid'
-    }, {
-      name: 'other',
-      ch_name: '其他',
-      icon:'https://fontawesome.com/icons/pen?style=solid'
-    })
-    
-    
-    
+  Category.create({
+    name: 'home',
+    ch_name: '家居物業',
+    icon: 'https://fontawesome.com/icons/home?style=solid'
+  }, {
+    name: 'transportation',
+    ch_name: '交通出行',
+    icon: 'https://fontawesome.com/icons/shuttle-van?style=solid'
+  }, {
+    name: 'entertainment',
+    ch_name: '休閒娛樂',
+    icon: 'https://fontawesome.com/icons/grin-beam?style=solid'
+  }, {
+    name: 'food',
+    ch_name: '餐飲食品',
+    icon: 'https://fontawesome.com/icons/utensils?style=solid'
+  }, {
+    name: 'other',
+    ch_name: '其他',
+    icon: 'https://fontawesome.com/icons/pen?style=solid'
+  })
+
   console.log('done')
 })
